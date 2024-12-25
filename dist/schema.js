0b88e833275d4802dd265023f7a792d84a6821b5
priyanshub18 <priyanshubhardwaj658@gmail.com>
2024-12-25 18:54:51 +0530

    first-bootstrap

---
+++ dist/schema.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+
+const tagSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+    },
+);
+
+const contentTypes = ['image', 'video', 'article', 'audio']; // Extend as needed
+
+const contentSchema = new Schema({
+  link: { type: String, required: true },
+  type: { type: String, enum: contentTypes, required: true },
+  title: { type: String, required: true },
+  tags: [{ type: Types.ObjectId, ref: 'Tag' }],
+  userId: { type: Types.ObjectId, ref: 'User', required: true },
+});
+
+
+const linkSchema = new Schema({
+    hash: { type: String, required: true },
+    userId: { type: Types.ObjectId, ref: 'User', required: true },
+});
+
+mongoose.model('User', userSchema);
+mongoose.model('Tag', tagSchema);
+mongoose.model('Content', contentSchema);   
+mongoose.model('Link', linkSchema);
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+//if it could not find the file for declration of the types in typescript then we should put install the @types/express file using npm install @types/express 
+const app = express();
+mongoose.connect(
+    "mongodb+srv://priyanshub18:prBh1234@server-1.iuaxz.mongodb.net/todo-app"
+  );
+
+app.post('/api/v1/signup', (req, res) => {
+  
+});
+
+
+app.post('/api/v1/signin', (req, res) => {
+  
+});
+
+
+app.get('/api/v1/content', (req, res) => {
+  
+});
+
+
+app.delete('/api/v1/content', (req, res) => {
+  
+});
+
+
+app.post('/api/v1/brain/share', (req, res) => {
+  
+});
+
+app.get('/api/v1/brain/:shareLink', (req, res) => {
+  
+});
+
